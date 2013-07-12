b17a5352b1bd6e4a4de9ab484d8dde68c2ab3226
lancy.fernandes <lancy@LancyNF-InteliMac.local>
2013-07-12 15:06:18 +0530

    intial commit

---
+++ MyProject/MyProjectTests/MyProjectTests.m
@@ -0,0 +1,36 @@
+//
+//  MyProjectTests.m
+//  MyProjectTests
+//
+//  Created by lancy.fernandes on 12/07/13.
+//  Copyright (c) 2013 Lancy Norbert Fernandes. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyProjectTests : XCTestCase
+
+@end
+
+@implementation MyProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
